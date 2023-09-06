61232b1d48281a53d27e0554705fab482f16c85b
Behnam <behnamborhani95@gmail.com>
2023-09-06 22:58:10 +0330

    feat: fetch characters from api

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,29 +1,31 @@
 import React from "react";
 import "./CharacterCard.scss";
 
-const CharacterCard = () => {
+const CharacterCard = ({ name, image, status, species, location, origin }) => {
    return (
       <div className="card">
          <div className="row">
             <div className="col-12">
                <div className="card__img">
-                  <img src="./character.jpeg" alt="" />
+                  <img src={image} alt={name} />
                </div>
             </div>
             <div className="col-12">
                <div className="card__body">
                   <div className="row">
                      <div className="col-12">
-                        <h2 className="character-name">Tony Galopagus</h2>
-                        <h3 className="charachter-status">Alive - Human</h3>
+                        <h2 className="character-name">{name}</h2>
+                        <h3 className="charachter-status">
+                           {status} - {species}
+                        </h3>
                      </div>
                      <div className="col-12">
                         <p className="title">Last known location:</p>
-                        <h2 className="info">Citadel of Ricks</h2>
+                        <h2 className="info">{location.name}</h2>
                      </div>
                      <div className="col-12">
-                        <p className="title">First seen in:</p>
-                        <h2 className="info">The Rickshank Rickdemption</h2>
+                        <p className="title">Origin:</p>
+                        <h2 className="info">{origin.name}</h2>
                      </div>
                   </div>
                </div>
--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,36 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Characters.scss";
 import CharacterCard from "../CharacterCard/CharacterCard";
+import axios from "axios";
 
 const Characters = () => {
+   const [characters, setCharacters] = useState([]);
+   useEffect(() => {
+      axios
+         .get("https://rickandmortyapi.com/api/character")
+         .then((response) => setCharacters(response.data.results));
+   }, []);
+   // console.log(characters);
+
    return (
       <section id="characters">
          <div className="container">
             <div className="row">
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
-               <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-                  <CharacterCard />
-               </div>
+               {characters.length &&
+                  characters.map((character) => (
+                     <div
+                        className="col-12 col-md-6 col-lg-4 col-xl-3"
+                        key={character.id}
+                     >
+                        <CharacterCard {...character} />
+                     </div>
+                  ))}
             </div>
          </div>
       </section>
