9ccf9a47d6da694db2dfa5a447e8ca11d0cbbd36
Behnam <behnamborhani95@gmail.com>
2023-09-07 19:14:45 +0330

    feat: add loader

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -2,22 +2,33 @@ import React from "react";
 import "./Characters.scss";
 import CharacterCard from "../CharacterCard/CharacterCard";
 import useCharacters from "../../hooks/useCharacters";
+import { RingLoader } from "react-spinners";
 
 const Characters = () => {
-   const { data } = useCharacters();
+   const { data, isLoading } = useCharacters();
 
    return (
       <section id="characters">
          <div className="container">
             <div className="row">
-               {data?.results?.map((character) => (
-                  <div
-                     className="col-12 col-md-6 col-lg-4 col-xl-3"
-                     key={character.id}
-                  >
-                     <CharacterCard {...character} />
+               {isLoading ? (
+                  <div className="col-12">
+                     <RingLoader
+                        color="#36d7b7"
+                        className="loader"
+                        size={100}
+                     />
                   </div>
-               ))}
+               ) : (
+                  data?.results?.map((character) => (
+                     <div
+                        className="col-12 col-md-6 col-lg-4 col-xl-3"
+                        key={character.id}
+                     >
+                        <CharacterCard {...character} />
+                     </div>
+                  ))
+               )}
             </div>
          </div>
       </section>
--- src/pages/CharacterDetails/CharacterDetails.js
+++ src/pages/CharacterDetails/CharacterDetails.js
@@ -5,39 +5,52 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { useParams } from "react-router-dom";
 import useCharacter from "../../hooks/useCharacter";
+import { RingLoader } from "react-spinners";
 
 const CharacterDetails = () => {
    const { characterID } = useParams();
-   const { data } = useCharacter(characterID);
+   const { data, isLoading } = useCharacter(characterID);
    console.log(data);
 
    return (
       <>
          <Navbar />
          <Header />
-         {/*  <section id="character-details">
+         <section id="character-details">
             <div className="container">
                <div className="row">
-                  <div className="col-12">
-                     <img src={character?.image} alt={character?.name} />
-                  </div>
-                  <div className="col-12">
-                        <h2 className="character-name">{character?.name}</h2>
-                        <h3 className="charachter-status">
-                           {character?.status} - {character?.species}
-                        </h3>
-                     </div>
+                  {isLoading ? (
                      <div className="col-12">
-                        <p className="title">Last known location:</p>
-                        <h2 className="info">{character?.location.name}</h2>
+                        <RingLoader
+                           color="#36d7b7"
+                           className="loader"
+                           size={100}
+                        />
                      </div>
-                     <div className="col-12">
-                        <p className="title">Origin:</p>
-                        <h2 className="info">{character?.origin.name}</h2>
-                     </div>
+                  ) : (
+                     <>
+                        <div className="col-12">
+                           <img src={data?.image} alt={data?.name} />
+                        </div>
+                        <div className="col-12">
+                           <h2 className="character-name">{data?.name}</h2>
+                           <h3 className="charachter-status">
+                              {data?.status} - {data?.species}
+                           </h3>
+                        </div>
+                        <div className="col-12">
+                           <p className="title">Last known location:</p>
+                           <h2 className="info">{data?.location.name}</h2>
+                        </div>
+                        <div className="col-12">
+                           <p className="title">Origin:</p>
+                           <h2 className="info">{data?.origin.name}</h2>
+                        </div>
+                     </>
+                  )}
                </div>
             </div>
-         </section> */}
+         </section>
          <Footer />
       </>
    );
