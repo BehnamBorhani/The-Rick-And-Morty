4de38385a7197c7b7e7b1c17db3bbed447411c4d
Behnam <behnamborhani95@gmail.com>
2023-09-09 15:18:22 +0330

    remove context because react query has it as built in

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./styles/App.scss";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
-import DataProvider from "./contexts/DataContext";
 
 function App() {
    const router = useRoutes(routes);
 
-   return <DataProvider>{router}</DataProvider>;
+   return <>{router}</>;
 }
 
 export default App;
