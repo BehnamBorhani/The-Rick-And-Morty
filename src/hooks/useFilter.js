f797754abfe71895061c1e0dc4d1ac541cb66886
Behnam <behnamborhani95@gmail.com>
2023-09-09 15:06:00 +0330

    update react query to V4 and use persist to cash data on local storage

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from "react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 const useFilter = (page, name, gender, status) => {
    const queryClient = useQueryClient();
