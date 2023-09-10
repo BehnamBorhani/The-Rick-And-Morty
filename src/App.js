d92275d77a7c32875d4979e6cafbb1345d724505
Behnam <behnamborhani95@gmail.com>
2023-09-10 09:23:06 +0330

    feat: add animation to characters card

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./styles/App.scss";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
    const router = useRoutes(routes);
-
+   Aos.init();
    return (
       <>
          {router}
